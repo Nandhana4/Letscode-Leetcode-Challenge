05028fb38391e3fdfbb909ceff711daab7f37c46
Nandhana4 <nandhanasathyan267@gmail.com>
2026-02-25 20:15:49 +0530

    Create TemperatureConversion.c

---
+++ DAY3/TemperatureConversion.c
@@ -0,0 +1,7 @@
+double* convertTemperature(double celsius, int* returnSize) {
+    double * arr=(double *)malloc(sizeof(double)*2);
+    arr[0]=celsius+273.15;
+    arr[1]=celsius*1.80+32.00;
+    *returnSize=2;
+    return arr;
+}

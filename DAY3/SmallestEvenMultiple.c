9d0e3221ba0f020fdeb97d4b64cbf05e33dbf453
Nandhana4 <nandhanasathyan267@gmail.com>
2026-02-25 20:19:25 +0530

    Create SmallestEvenMultiple.c

---
+++ DAY3/SmallestEvenMultiple.c
@@ -0,0 +1,11 @@
+Int smallestEvenMultiple(int n) {
+   int i=1;
+    int x;
+    while(1){
+        if((i%2==0)&&(i%n==0)){
+           x=i; 
+            break;
+        } i++;
+        
+    } return x;
+}
